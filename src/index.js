020d12c12dadc63ee2e652fe334c54b186198ff4
Ondřej Moravec <moravec@devground.cz>
2024-05-28 12:59:04 +0200

    get three.js arrow tutorial

--- src/index.js
+++ src/index.js
@@ -1,36 +1,52 @@
 import './styles/main.css';
 import * as THREE from 'three';
+import WebGL from 'three/addons/capabilities/WebGL.js';
 
-const scene = new THREE.Scene();
-
-let fov = 75;
+let fov = 45;
 let aspect = window.innerWidth / window.innerHeight;
 let near = 0.1;
 let far = 1000;
 
-const camera = new THREE.PerspectiveCamera(
-    fov,
-    aspect,
-    near,
-    far
-);
+const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
+
+camera.position.set( 0, 0, 100 );
+camera.lookAt( 0, 0, 0 );
+
+const scene = new THREE.Scene();
+
+//create a blue LineBasicMaterial
+const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+
+const points = [];
+points.push( new THREE.Vector3( - 10, 0, 0 ) );
+points.push( new THREE.Vector3( 0, 10, 0 ) );
+points.push( new THREE.Vector3( 10, 0, 0 ) );
+
+const geometry = new THREE.BufferGeometry().setFromPoints( points );
+
+const line = new THREE.Line( geometry, material );
+scene.add( line );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
-camera.position.z = 5;
-
 function animate() {
 	requestAnimationFrame( animate );
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-	renderer.render( scene, camera );
+
+    // TODO: Add your code here
+
+    if (renderer !== undefined) {
+	    renderer.render( scene, camera );
+    }
+}
+
+if ( WebGL.isWebGLAvailable() ) {
+	// Initiate function or other initializations here
+	animate();
+} else {
+	const warning = WebGL.getWebGLErrorMessage();
+	document.getElementById( 'container' ).appendChild( warning );
 }
 
 animate();
