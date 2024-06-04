7ae82a2f189d73caa9c8f279f1adfca521b49387
Ondřej Moravec <moravec@devground.cz>
2024-06-04 15:07:22 +0200

    trying to solve resizing :(

--- src/utils/app.js
+++ src/utils/app.js
@@ -1,16 +1,17 @@
-import * as THREE from 'three';
-import Stats from 'three/addons/libs/stats.module.js';
-import WebGL from 'three/addons/capabilities/WebGL.js';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import * as THREE from "three";
+import Stats from "three/addons/libs/stats.module.js";
+import WebGL from "three/addons/capabilities/WebGL.js";
+import { SDFGeometryGenerator } from "three/addons/geometries/SDFGeometryGenerator.js";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer.js";
 
-import vs from '.././shaders/raw1.vert';
-import fs from '.././shaders/raw1.frag';
-import hm from '.././textures/map.png';
+import vs from ".././shaders/raw1.vert";
+import fs from ".././shaders/raw1.frag";
+import hm from ".././textures/map.png";
 
-import { DG_Plane } from '.';
+import { DG_Plane } from ".";
 
 class App {
-    
     constructor(antialias = true, axisHelper = false) {
         this.bg_color = 0x110022;
         this.antialias = antialias;
@@ -28,20 +29,24 @@ class App {
 
         // Set up the renderer
         this.renderer = new THREE.WebGLRenderer({ antialias: this.antialias });
+        this.renderer.setPixelRatio(window.devicePixelRatio);
         this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.renderer.shadowMap.enabled = true;
         document.body.appendChild(this.renderer.domElement);
+
+        this.composer = new EffectComposer(this.renderer);
 
         // Set up the axis helper
         if (this.useAxisHelper) {
             this.axisHelper = new THREE.AxesHelper(this.axisHelperSize);
             this.scene.add(this.axisHelper);
-        };
-        
+        }
+
         // Set up the clock
         this.clock = new THREE.Clock();
 
         // Set up the stats
-        const container = document.createElement('div');
+        const container = document.createElement("div");
         document.body.appendChild(container);
         this.stats = new Stats();
         container.appendChild(this.stats.dom);
@@ -52,11 +57,54 @@ class App {
         this.camera.lookAt(0, 0, 0);
 
         // Set up the controls
-        this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+        this.controls = new OrbitControls(
+            this.camera,
+            this.renderer.domElement
+        );
         this.controls.enableDamping = true;
         this.controls.dampingFactor = 0.25;
         this.controls.enableZoom = true;
-    };
+
+        //window.addEventListener("resize", this.onWindowResize(this));
+    }
+
+    #onWindowResize(app) {
+        if (!(app instanceof App)) {
+            console.error("The app parameter must be an instance of App");
+            return;
+        }
+
+        // const ASPECT_RATIO = window.innerWidth / window.innerHeight;
+        // const WIDTH = window.innerWidth * window.devicePixelRatio;
+        // const HEIGHT = window.innerHeight * window.devicePixelRatio;
+
+        // app.aspect = ASPECT_RATIO;
+        // if (app.camera) {
+        //     if (app.camera.isPerspectiveCamera) {
+        //         app.camera.aspect = ASPECT_RATIO;
+        //         app.camera.updateProjectionMatrix();
+        //     } else if (app.camera.isOrthographicCamera) {
+        //         app.camera.left = 0;
+        //         app.camera.right = WIDTH;
+        //         app.camera.top = 0;
+        //         app.camera.bottom = HEIGHT;
+        //         app.camera.updateProjectionMatrix();
+        //     }
+        // }
+
+        this.resizeRendererToDisplaySize(app.renderer);
+    }
+
+    #resizeRendererToDisplaySize(renderer) {
+        const canvas = renderer.domElement;
+        const width = canvas.clientWidth;
+        const height = canvas.clientHeight;
+        const needResize = canvas.width !== width || canvas.height !== height;
+        if (needResize) {
+            renderer.setSize(width, height, false);
+        }
+        return needResize;
+    }
 
     start() {
         if (WebGL.isWebGLAvailable()) {
@@ -67,58 +115,72 @@ class App {
         } else {
             this.running = false;
             const warning = WebGL.getWebGLErrorMessage();
-            const warningContainer = document.createElement('div');
+            const warningContainer = document.createElement("div");
             document.body.appendChild(warningContainer);
             warningContainer.appendChild(warning);
         }
     }
 
-    #animate() {        
+    #animate() {
         if (this.running == false) return;
 
         requestAnimationFrame(this.#animate.bind(this));
 
         const delta = this.clock.getDelta();
-        
+
+        if (this.#resizeRendererToDisplaySize(this.renderer)) {
+            const canvas = renderer.domElement;
+            this.camera.aspect = canvas.clientWidth / canvas.clientHeight;
+            this.camera.updateProjectionMatrix();
+        }
+
         // update the uniforms
-        this.uniforms.iTime.value += delta;
-        this.uniforms.iTimeDelta.value = delta;
+        // this.uniforms.iTime.value = this.clock.elapsedTime;
+        // this.uniforms.iTimeDelta.value = delta;
 
         this.stats.update();
         this.renderer.render(this.scene, this.camera);
     }
 
     #loadDefaultScene() {
-        // Set up the geometry
-        const scale = new THREE.Vector2(16, 16);
-        const dgPlane = new DG_Plane(
-            new THREE.Vector3(0, 0, 0),
-            new THREE.Vector3(scale.x / 2, 0, scale.y / 2),
-            scale,
-            new THREE.Vector2(1024, 1024)
-        );
-        const geometry = dgPlane.geometry;
-        geometry.dynamic = true;
+        const generator = new SDFGeometryGenerator(this.renderer);
+        const sdf = /*glsl*/ `float dist(vec3 p) {return length(p) - 0.5;}`;
+        const geometry = generator.generate(Math.pow(2, 6), sdf, 1); // ~> THREE.BufferGeometry
+        let meshFromSDF;
 
-        // This is the uniforms
-        this.uniforms = { 
-            iTime: { value: 0.0 },
-            iTimeDelta: { value: 0.0 },
-            iResolution: { value: new THREE.Vector3(window.innerWidth, window.innerHeight, 1) },
-            uvScale: { value: new THREE.Vector2(1, 1) },
-            iChannel0: { type: 't', value: new THREE.TextureLoader().load(hm) }
-        } 
+        if (meshFromSDF) {
+            // updates mesh
 
-        // This is the shader material
-        const myMeshBasicMaterial = new THREE.RawShaderMaterial({
-            uniforms: this.uniforms,
-            vertexShader: vs,
-            fragmentShader: fs,
-            glslVersion: THREE.GLSL3
-        })
+            meshFromSDF.geometry.dispose();
+            meshFromSDF.geometry = geometry;
+        } else {
+            // inits meshFromSDF : THREE.Mesh
 
-        const plane = new THREE.Mesh(geometry, myMeshBasicMaterial);
-        this.scene.add(plane);
+            meshFromSDF = new THREE.Mesh(
+                geometry,
+                new THREE.MeshStandardMaterial()
+            );
+            this.scene.add(meshFromSDF);
+
+            const scale =
+                (Math.min(window.innerWidth, window.innerHeight) * 1.618) / 2;
+            meshFromSDF.scale.set(scale, scale, scale);
+            meshFromSDF.position.set(0, 0, 0);
+
+            meshFromSDF.material.dispose();
+            meshFromSDF.material = new THREE.MeshDepthMaterial({
+                // depthPacking: THREE.RGBADepthPacking,
+                // map: new THREE.TextureLoader().load( hm ),
+                // displacementMap: new THREE.TextureLoader().load( hm ),
+                // displacementScale: 0.1,
+                // displacementBias: -0.1,
+                // depthTest: true,
+                // depthWrite: true,
+                // side: THREE.DoubleSide,
+                // transparent: false,
+                // wireframe: false
+            });
+        }
     }
 }
 
